84615650f024dda411eadcd64c1a26c015042fa9
Vitor Pereira Resende <vitorpereiraresende26@gmail.com>
2024-01-15 15:24:33 -0300

    finished what I need to know

--- src/If.kt
+++ src/If.kt
@@ -2,10 +2,29 @@ fun main(){
     val n1 = 10;
     val n2 = 20;
 
-    if(n1 > n2){
+    // true
+    if(n1 > n2 && n1 != 0 || n2 < 30){
         println(true);
     }
     else {
         println(false);
     }
+
+    // false
+    if(n1 > n2 && (n1 != 0 || n2 < 30)){
+        println(true);
+    }
+    else if(n2 < 80 && n1 != 0){
+        println(false);
+    }
+    else {
+        println("nothing")
+    }
+
+    val n3 = 1;
+    val n4 = 2;
+
+    val result = if (n3 >= n4) "$n3 - $n4 = ${(n3 - n4)}" else "$n3 + $n4 = ${(n3 + n4)}";
+
+    println(result);
 }
